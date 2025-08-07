f1222f64e31918c5fecb9dcda5e5430c4c96ffc7
pvgit <pvgit@PVG.LOCAL>
2025-08-07 10:32:49 +0530

    first commit

---
+++ PRAC7.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, classification_report
+
+X, y = load_iris(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=42)
+
+nb_model = GaussianNB().fit(X_train,y_train)
+
+y_pred = nb_model.predict(X_test)
+
+accuracy = accuracy_score(y_test,y_pred)
+print("Accuracy:", accuracy)
+print("Classification Report:\n", classification_report(y_test,y_pred))
